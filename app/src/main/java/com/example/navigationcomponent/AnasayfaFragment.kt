3546fd142ba1ee141009fb1c63bc415fd8547071
Mürsel <murselcetin51@gmail.com>
2022-06-17 09:37:36 +0300

    Ders 6 İşlendi.

--- app/src/main/java/com/example/navigationcomponent/AnasayfaFragment.kt
+++ app/src/main/java/com/example/navigationcomponent/AnasayfaFragment.kt
@@ -5,9 +5,23 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.example.navigationcomponent.databinding.FragmentAnasayfaBinding
+import com.google.android.material.snackbar.Snackbar
 
 class AnasayfaFragment : Fragment() {
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        return inflater.inflate(R.layout.fragment_anasayfa, container, false)
+    private lateinit var tasarim: FragmentAnasayfaBinding
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        tasarim = FragmentAnasayfaBinding.inflate(inflater, container, false)
+
+        tasarim.buttonBasla.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.oyunEkraninaGecis)
+        }
+
+        return tasarim.root
     }
 }
